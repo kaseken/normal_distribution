a7a7598d0ff9c117cc70710cd7f6f634f2b3a43c
kaseken <k.kaseda0925@gmail.com>
2022-05-29 22:43:57 +0900

    refactor: minor refactoring

--- lib/src/normal_distribution_base.dart
+++ lib/src/normal_distribution_base.dart
@@ -11,7 +11,7 @@ class NormalDistribution {
     required this.sigma,
   });
 
-  double z({required double x}) => (x - mean) / sigma;
+  double _z({required double x}) => (x - mean) / sigma;
 
   double pdf({required double x}) {
     final dividiend = pow(
@@ -23,7 +23,7 @@ class NormalDistribution {
   }
 
   double cdf({required double x}) {
-    final z = round(this.z(x: x), decimalPlaces: 2);
+    final z = round(_z(x: x), decimalPlaces: 2);
     if (z == 0) {
       return 0.5;
     }
@@ -33,22 +33,14 @@ class NormalDistribution {
     if (z >= 3.5) {
       return 1;
     }
-    final absZScore = z.abs();
-    final zRow = (absZScore * 10).floor() / 10;
-    final zCol = round(
-      ((absZScore * 100).round() % 10) / 100,
-      decimalPlaces: 2,
-    );
-    final zColIndex = indexOfZTable(zCol);
-    final absPercentile = zTable[zRow]?[zColIndex];
+    final zRow = (z.abs() * 10).floor() / 10;
+    final zCol = (z.abs() * 100).round() % 10;
+    final absPercentile = zTable[zRow]?[zCol];
     if (absPercentile == null) {
       throw Exception('Invalid zTable');
     }
     return z < 0 ? 1 - absPercentile : absPercentile;
   }
-
-  double probabilityBetween({required double x1, required double x2}) =>
-      (cdf(x: x1) - cdf(x: x2)).abs();
 }
 
 double round(double value, {required int decimalPlaces}) {
