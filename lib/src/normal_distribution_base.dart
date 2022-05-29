5c8cce6cc72d148136029e25618ba3d036bc1051
kaseken <k.kaseda0925@gmail.com>
2022-05-29 21:28:36 +0900

    feat: add zTable

--- lib/src/normal_distribution_base.dart
+++ lib/src/normal_distribution_base.dart
@@ -11,12 +11,19 @@ class NormalDistribution {
 
   double zScore({required double value}) => (value - mean) / standardDeviation;
 
-  double pdf({required double value}) {
+  double pdf({required double x}) {
     final dividiend = pow(
       e,
-      -(pow(value - mean, 2.0) / (2 * pow(standardDeviation, 2.0))),
+      -(pow(x - mean, 2.0) / (2 * pow(standardDeviation, 2.0))),
     );
     final divisor = standardDeviation * sqrt(2 * pi);
     return dividiend / divisor;
   }
+
+  double cdf({required double x}) {
+    throw UnimplementedError();
+  }
+
+  double probabilityBetween({required double x1, required double x2}) =>
+      (cdf(x: x1) - cdf(x: x2)).abs();
 }
--- test/normal_distribution_test.dart
+++ test/normal_distribution_test.dart
@@ -1,16 +1,5 @@
-import 'package:normal_distribution/normal_distribution.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
-
-    setUp(() {
-      // Additional setup goes here.
-    });
-
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
-  });
+  group('', () {});
 }
