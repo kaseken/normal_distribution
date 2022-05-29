d1342eba611d2a9bb4c63b5344b79e77958d2ff9
kaseken <k.kaseda0925@gmail.com>
2022-05-29 21:08:10 +0900

    feat: add pdf

--- lib/src/normal_distribution_base.dart
+++ lib/src/normal_distribution_base.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 class NormalDistribution {
   final double mean;
   final double standardDeviation;
@@ -8,4 +10,13 @@ class NormalDistribution {
   });
 
   double zScore({required double value}) => (value - mean) / standardDeviation;
+
+  double pdf({required double value}) {
+    final dividiend = pow(
+      e,
+      -(pow(value - mean, 2.0) / (2 * pow(standardDeviation, 2.0))),
+    );
+    final divisor = standardDeviation * sqrt(2 * pi);
+    return dividiend / divisor;
+  }
 }
