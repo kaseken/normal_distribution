65b40f34fa91c22ba225a6e0fc001c513ac2164e
kaseken <k.kaseda0925@gmail.com>
2022-05-29 22:39:04 +0900

    refactor: rename zScore

--- lib/src/normal_distribution_base.dart
+++ lib/src/normal_distribution_base.dart
@@ -11,7 +11,7 @@ class NormalDistribution {
     required this.sigma,
   });
 
-  double zScore({required double x}) => (x - mean) / sigma;
+  double z({required double x}) => (x - mean) / sigma;
 
   double pdf({required double x}) {
     final dividiend = pow(
@@ -23,17 +23,17 @@ class NormalDistribution {
   }
 
   double cdf({required double x}) {
-    final zScore = round(this.zScore(x: x), decimalPlaces: 2);
-    if (zScore == 0) {
+    final z = round(this.z(x: x), decimalPlaces: 2);
+    if (z == 0) {
       return 0.5;
     }
-    if (zScore <= -3.5) {
+    if (z <= -3.5) {
       return 0;
     }
-    if (zScore >= 3.5) {
+    if (z >= 3.5) {
       return 1;
     }
-    final absZScore = zScore.abs();
+    final absZScore = z.abs();
     final zRow = (absZScore * 10).floor() / 10;
     final zCol = round(
       ((absZScore * 100).round() % 10) / 100,
@@ -44,7 +44,7 @@ class NormalDistribution {
     if (absPercentile == null) {
       throw Exception('Invalid zTable');
     }
-    return zScore < 0 ? 1 - absPercentile : absPercentile;
+    return z < 0 ? 1 - absPercentile : absPercentile;
   }
 
   double probabilityBetween({required double x1, required double x2}) =>
